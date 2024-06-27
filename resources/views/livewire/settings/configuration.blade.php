02c2d7f4ac26e6b51abf894e0fcf8569fa614689
Benjamin Rumble <therumbler@gmail.com>
2024-06-26 21:34:19 -0400

    fix small typo in configuration.blade.php
    againts -> against

--- resources/views/livewire/settings/configuration.blade.php
+++ resources/views/livewire/settings/configuration.blade.php
@@ -13,7 +13,7 @@
                 <x-forms.input id="settings.fqdn" label="Instance's Domain" placeholder="https://coolify.io" />
                 <x-forms.input id="settings.instance_name" label="Instance's Name" placeholder="Coolify" />
                 <x-forms.input id="settings.custom_dns_servers" label="DNS Servers"
-                    helper="DNS servers for validation FQDNs againts. A comma separated list of DNS servers."
+                    helper="DNS servers for validation FQDNs against. A comma separated list of DNS servers."
                     placeholder="1.1.1.1,8.8.8.8" />
                 <div class="md:w-96">
                     <x-forms.checkbox instantSave id="is_dns_validation_enabled" label="Validate DNS settings?" />
